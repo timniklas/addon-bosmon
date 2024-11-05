499ebbdc82e0825ab4d999871c973b2421bb9b0e
timniklas <accounts@tnkoch.de>
2024-11-05 17:35:55 +0100

    Update start-vnc.sh

--- bosmon/start-vnc.sh
+++ bosmon/start-vnc.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo 'Write license data to /tmp'
-echo $(bashio::config 'key_name') > /tmp/key_name
-echo $(bashio::config 'key_serial') > /tmp/key_serial
-
 echo 'Updating /etc/hosts file...'
 HOSTNAME=$(hostname)
 echo "127.0.1.1\t$HOSTNAME" >> /etc/hosts
