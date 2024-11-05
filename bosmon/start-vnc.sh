c57ae441e75eeb0ad30441e3ca16aa1b69cfb224
timniklas <accounts@tnkoch.de>
2024-11-05 13:55:59 +0100

    Update start-vnc.sh

--- bosmon/start-vnc.sh
+++ bosmon/start-vnc.sh
@@ -10,19 +10,4 @@ vncserver -geometry $RESOLUTION -SecurityTypes None &
 echo "VNC server started at $RESOLUTION! ^-^"
 
 echo "Starting novnc..."
-websockify -D --web=/usr/share/novnc/ 8099 localhost:5901
-
-echo "Waiting for BosMon to start..."
-# Wait for the notification daemon to finish launching
-while ! pgrep -f "xstartup" > /dev/null; do
-
-    # Set optional delay
-    sleep .1
-done
-
-echo "Startup complete"
-while pgrep -f "xstartup" > /dev/null; do
-
-    # Set optional delay
-    sleep .1
-done
+websockify --web=/usr/share/novnc/ 8099 localhost:5901
