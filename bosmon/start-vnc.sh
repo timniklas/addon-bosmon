3f3de789823b00784a3907b490651b9982c8111a
timniklas <accounts@tnkoch.de>
2024-11-06 15:06:38 +0100

    Update start-vnc.sh

--- bosmon/start-vnc.sh
+++ bosmon/start-vnc.sh
@@ -4,16 +4,16 @@ echo 'Updating /etc/hosts file...'
 HOSTNAME=$(hostname)
 echo "127.0.1.1\t$HOSTNAME" >> /etc/hosts
 
-echo "Starting VNC server at $RESOLUTION..."
-vncserver -geometry $RESOLUTION -SecurityTypes None &
-
-echo "VNC server started at $RESOLUTION! ^-^"
-
 echo "Linking persistent storage..."
 mkdir -p "/root/.wine/drive_c/users/root/AppData/Roaming"
 ln -sT /data/appdata "/root/.wine/drive_c/users/root/AppData/Roaming/BosMon"
 mkdir -p "/root/.wine/drive_c/Program Files"
 ln -sT /data/program "/root/.wine/drive_c/Program Files/BosMon"
+
+echo "Starting VNC server at $RESOLUTION..."
+vncserver -geometry $RESOLUTION -SecurityTypes None &
+
+echo "VNC server started at $RESOLUTION! ^-^"
 
 echo "Starting novnc..."
 websockify --web=/usr/share/novnc/ 8099 localhost:5901
