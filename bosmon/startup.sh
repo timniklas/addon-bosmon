2a7aa94a13e86807fc3c8e31b198c98a138d3677
timniklas <accounts@tnkoch.de>
2024-11-06 14:21:32 +0100

    Update startup.sh

--- bosmon/startup.sh
+++ bosmon/startup.sh
@@ -2,13 +2,6 @@
 
 x-window-manager &
 
-# Link persistent storage 1
-mkdir -p /data/program
-ln -sT /data/program "/root/.wine/drive_c/users/root/AppData/Roaming/BosMon"
-# Link persistent storage 2
-mkdir -p /data/appdata
-ln -sT /data/appdata "/root/.wine/drive_c/Program Files/BosMon"
-
 # Install BosMon
 if [ ! -f "/root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe" ]; then
     ./install_bosmon.sh &
