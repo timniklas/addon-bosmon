fae826c246dc9eaeb23991ed28ebe11cf2c5a214
timniklas <accounts@tnkoch.de>
2024-11-06 11:11:40 +0100

    Update startup.sh

--- bosmon/startup.sh
+++ bosmon/startup.sh
@@ -8,4 +8,9 @@ if [ ! -f "/root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe" ]; then
     wine bosmon_setup.exe /silent /COMPONENTS=bosmon
 fi
 
-wine /root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe
+# Keep BosMon running
+while true
+do
+    wine /root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe
+    sleep 1
+done
