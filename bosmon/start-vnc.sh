410251efd442a6b7afcd6e91eefbd9883958d01a
timniklas <accounts@tnkoch.de>
2024-11-06 16:26:41 +0100

    Update start-vnc.sh

--- bosmon/start-vnc.sh
+++ bosmon/start-vnc.sh
@@ -8,9 +8,6 @@ echo "Linking persistent storage..."
 mkdir -p /data/appdata
 mkdir -p "/root/.wine/drive_c/users/root/AppData/Roaming"
 ln -sT /data/appdata "/root/.wine/drive_c/users/root/AppData/Roaming/BosMon"
-mkdir -p /data/program
-mkdir -p "/root/.wine/drive_c/Program Files"
-ln -sT /data/program "/root/.wine/drive_c/Program Files/BosMon"
 
 echo "Starting VNC server at $RESOLUTION..."
 vncserver -geometry $RESOLUTION -SecurityTypes None &
