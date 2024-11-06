d731cc0e76e53e81e94091c43e0fa1d1987084a6
timniklas <accounts@tnkoch.de>
2024-11-06 16:10:12 +0100

    Update startup.sh

--- bosmon/startup.sh
+++ bosmon/startup.sh
@@ -3,9 +3,12 @@
 x-window-manager &
 
 # Install BosMon
-if [ ! -f "/root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe" ]; then
+if [ ! -f /root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe ]; then
     ./install_bosmon.sh &
     wine bosmon_setup.exe /silent /COMPONENTS=bosmon
+else
+    ./install_bosmon.sh &
+    wine /root/.wine/drive_c/Program\ Files/BosMon/Activate.exe
 fi
 
 while true
