bf408fba2b0d334589ec8c51530c65ed115cac3c
timniklas <accounts@tnkoch.de>
2025-06-09 01:04:55 +0200

    start full desktop

--- xstartup.sh
+++ xstartup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+CONFIG_PATH=/data/options.json
+RESOLUTION=$(jq --raw-output '.display_resolution // empty' $CONFIG_PATH)
+
 x-window-manager &
 
 while true
 do
-    wine /root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe
+    wine explorer /desktop=BosMon,$RESOLUTION "C:\\Program Files\\BosMon\\BosMon.exe"
     sleep 1
 done
