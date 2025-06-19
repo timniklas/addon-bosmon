b3bf4a896a51fe8be06f1ffd766c81cfcd0273b5
timniklas <accounts@tnkoch.de>
2025-06-19 10:22:36 +0200

    Update patch_novnc.sh

--- patch_novnc.sh
+++ patch_novnc.sh
@@ -2,8 +2,8 @@
 
 filename="/usr/share/novnc/app/ui.js"
 
-find="UI.initSetting('path', 'var path = UI.getSetting('path');"
-replace="UI.initSetting('path', 'var path = window.location.pathname.slice(1) + UI.getSetting('path');"
+find="var path = UI.getSetting('path');"
+replace="var path = window.location.pathname.slice(1) + UI.getSetting('path');"
 sed -i "s/$find/$replace/g" $filename
 
 find="var autoconnect = WebUtil.getConfigVar('autoconnect', false);"
