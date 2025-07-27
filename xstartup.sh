70f8a3aceaf745027e365a0fabf641b13b123810
timniklas <accounts@tnkoch.de>
2025-07-27 13:59:00 +0200

    Update xstartup.sh

--- xstartup.sh
+++ xstartup.sh
@@ -2,20 +2,14 @@
 
 CONFIG_PATH=/data/options.json
 RESOLUTION=$(jq --raw-output '.display_resolution // empty' $CONFIG_PATH)
-DESKTOP_WIDTH=$(echo $RESOLUTION | cut -d'x' -f1)
-DESKTOP_HEIGHT=$(echo $RESOLUTION | cut -d'x' -f2)
 
-xfwm4 &
-
-# Setze Emulated Desktop und Größe
-wine reg add "HKCU\\Software\\Wine\\Explorer" /v Desktop /t REG_SZ /d "${DESKTOP_WIDTH}x${DESKTOP_HEIGHT}" /f
-
-# Deaktiviere "Allow the window manager to control the windows"
+# Setze Wine-Konfiguration automatisch (wie vorher besprochen)
+wine reg add "HKCU\\Software\\Wine\\Explorer" /v Desktop /t REG_SZ /d "${RESOLUTION}" /f
 wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v Managed /t REG_SZ /d "N" /f
 
+# Starte NUR Wine mit Emulated Desktop, OHNE Window-Manager
 while true
 do
-    #wine /root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe
     wine explorer /desktop=BosMon,$RESOLUTION /root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe
     sleep 1
 done
