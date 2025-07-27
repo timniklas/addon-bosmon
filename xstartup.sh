2eaa3e9ef8b4ed3da948c17f3d88d9e30f620ec9
timniklas <accounts@tnkoch.de>
2025-07-27 14:28:57 +0200

    Update xstartup.sh

--- xstartup.sh
+++ xstartup.sh
@@ -3,13 +3,13 @@
 CONFIG_PATH=/data/options.json
 RESOLUTION=$(jq --raw-output '.display_resolution // empty' $CONFIG_PATH)
 
-# Setze Wine-Konfiguration automatisch (wie vorher besprochen)
-wine reg add "HKCU\\Software\\Wine\\Explorer" /v Desktop /t REG_SZ /d "${RESOLUTION}" /f
-wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v Managed /t REG_SZ /d "N" /f
+wine reg add "HKCU\\Software\\Wine\\X11 Driver" /v Managed /t REG_SZ /d "Y" /f
+wine reg add "HKCU\\Control Panel\\Desktop" /v LogPixels /t REG_DWORD /d 96 /f
 
-# Starte NUR Wine mit Emulated Desktop, OHNE Window-Manager
+xfwm4 &
+
 while true
 do
-    wine explorer /desktop=BosMon,$RESOLUTION /root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe
+    wine /root/.wine/drive_c/Program\ Files/BosMon/BosMon.exe
     sleep 1
 done
